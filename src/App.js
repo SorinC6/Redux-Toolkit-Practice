36015017cf074faa4d783b27f4ea7ca8970c300e
SorinC6 <kish.sorin@yahoo.com>
2022-09-06 14:39:03 +0300

    add form

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import './App.css';
-import Counter from './features/counter/Counter';
+
+import PostList from './features/posts/postsList'
+import AddPostForm from './features/posts/addPostForm'
 
 function App() {
   return (
     <div className="App">
-      <Counter />
+      <AddPostForm />
+      <PostList />
     </div>
   );
 }
